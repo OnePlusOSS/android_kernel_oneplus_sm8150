c72c25c9a40e5f9413bbf0ee97822e6ecd726f8c
kim.ba <kim.ba@oneplus.com>
2019-10-28 14:09:23 +0800

    fix the kernel panic on headset.
    Change-Id: Iec91c6aa8c86c9dba8dcf49af1f7837bbef0891a

--- sound/usb/usb_audio_qmi_svc.c
+++ sound/usb/usb_audio_qmi_svc.c
@@ -872,34 +872,32 @@ static void uaudio_disconnect_cb(struct snd_usb_audio *chip)
 		goto done;
 	}
 
-	if (atomic_read(&dev->in_use)) {
-		mutex_unlock(&chip->dev_lock);
-		uaudio_dbg("sending qmi indication disconnect\n");
-		uaudio_dbg("sq->sq_family:%x sq->sq_node:%x sq->sq_port:%x\n",
-				svc->client_sq.sq_family,
-				svc->client_sq.sq_node, svc->client_sq.sq_port);
-		disconnect_ind.dev_event = USB_AUDIO_DEV_DISCONNECT_V01;
-		disconnect_ind.slot_id = dev->udev->slot_id;
-		disconnect_ind.controller_num = dev->usb_core_id;
-		disconnect_ind.controller_num_valid = 1;
-		ret = qmi_send_indication(svc->uaudio_svc_hdl, &svc->client_sq,
-				QMI_UADUIO_STREAM_IND_V01,
-				QMI_UAUDIO_STREAM_IND_MSG_V01_MAX_MSG_LEN,
-				qmi_uaudio_stream_ind_msg_v01_ei,
-				&disconnect_ind);
-		if (ret < 0) {
-			uaudio_err("qmi send failed wiht err: %d\n", ret);
-			return;
-		}
-
-		ret = wait_event_interruptible(dev->disconnect_wq,
-				!atomic_read(&dev->in_use));
-		if (ret < 0) {
-			uaudio_dbg("failed with ret %d\n", ret);
-			return;
-		}
-		mutex_lock(&chip->dev_lock);
+	mutex_unlock(&chip->dev_lock);
+	uaudio_dbg("sending qmi indication disconnect\n");
+	uaudio_dbg("sq->sq_family:%x sq->sq_node:%x sq->sq_port:%x\n",
+			svc->client_sq.sq_family,
+			svc->client_sq.sq_node, svc->client_sq.sq_port);
+	disconnect_ind.dev_event = USB_AUDIO_DEV_DISCONNECT_V01;
+	disconnect_ind.slot_id = dev->udev->slot_id;
+	disconnect_ind.controller_num = dev->usb_core_id;
+	disconnect_ind.controller_num_valid = 1;
+	ret = qmi_send_indication(svc->uaudio_svc_hdl, &svc->client_sq,
+			QMI_UADUIO_STREAM_IND_V01,
+			QMI_UAUDIO_STREAM_IND_MSG_V01_MAX_MSG_LEN,
+			qmi_uaudio_stream_ind_msg_v01_ei,
+			&disconnect_ind);
+	if (ret < 0) {
+		uaudio_err("qmi send failed wiht err: %d\n", ret);
+		return;
 	}
+
+	ret = wait_event_interruptible(dev->disconnect_wq,
+			!atomic_read(&dev->in_use));
+	if (ret < 0) {
+		uaudio_dbg("failed with ret %d\n", ret);
+		return;
+	}
+	mutex_lock(&chip->dev_lock);
 
 	uaudio_dev_cleanup(dev);
 done:
@@ -1156,17 +1154,17 @@ static void handle_uaudio_stream_req(struct qmi_handle *handle,
 
 response:
 	if (!req_msg->enable && ret != -EINVAL) {
+		mutex_lock(&chip->dev_lock);
 		if (info_idx >= 0) {
-			mutex_lock(&chip->dev_lock);
 			info = &uadev[pcm_card_num].info[info_idx];
 			uaudio_dev_intf_cleanup(uadev[pcm_card_num].udev, info);
 			uaudio_dbg("release resources: intf# %d card# %d\n",
 					subs->interface, pcm_card_num);
-			mutex_unlock(&chip->dev_lock);
 		}
 		if (atomic_read(&uadev[pcm_card_num].in_use))
 			kref_put(&uadev[pcm_card_num].kref,
 					uaudio_dev_release);
+		mutex_unlock(&chip->dev_lock);
 	}
 
 	resp.usb_token = req_msg->usb_token;
